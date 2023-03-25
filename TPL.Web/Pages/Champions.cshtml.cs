478bdc6bcb2de5d327839c2a8ebd6f9a9218e866
Corey Buzzell <cdbuzzell@gmail.com>
2023-03-25 12:05:52 -0500

    Champions page showing data

---
+++ TPL.Web/Pages/Champions.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TPL.Model;
+
+namespace TPL.Web.Pages
+{
+    public class ChampionsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public List<SeasonChampion> Champions { get; set; }
+
+        public ChampionsModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _config = configuration;
+        }
+
+        public void OnGet()
+        {
+            string connectionString = _config["Data:DefaultConnection:ConnectionString"];
+            Logic bl = new Logic(connectionString);
+
+            Champions = bl.GetChampions(Convert.ToInt32(_config["AppSettings:CurrentSeason"]));
+        }
+    }
+}
