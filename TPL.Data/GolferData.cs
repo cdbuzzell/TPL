49ed1b5340ea95b036def967f09a23dbcee74a9f
Corey Buzzell <cdbuzzell@gmail.com>
2023-03-24 21:48:34 -0500

    Copied over remaining (golfer) data access methods

---
+++ TPL.Data/GolferData.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPL.Model;
+
+namespace TPL.Data
+{
+    public class GolferData
+    {
+        string _connectionString = string.Empty;
+
+        public GolferData(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<GolferRound> GetRoundGolfers(Guid roundId)
+        {
+            List<GolferRound> roundGolfers = new List<GolferRound>();
+
+            //TODO: finish
+
+            return roundGolfers;
+        }
+
+        public List<GolferRound> GetGolferRounds(string alias, int season)
+        {
+            List<GolferRound> seasonRounds = new List<GolferRound>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetGolferRounds", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Golfer", alias);
+                    command.Parameters.AddWithValue("@Season", season);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            seasonRounds.Add(new GolferRound
+                            {
+                                GolferRoundId = reader["GolferRoundId"] == DBNull.Value ? Guid.Empty : (Guid)reader["GolferRoundId"],
+                                Round = new Round
+                                {
+                                    RoundId = (Guid)reader["RoundId"],
+                                    Date = Convert.ToDateTime(reader["Date"]),
+                                    Name = reader["RoundName"].ToString(),
+                                    Details = reader["Details"].ToString(),
+                                    Game = reader["Game"].ToString(),
+                                    IsMajor = reader["IsMajor"] == DBNull.Value ? false : (bool)reader["IsMajor"]
+                                },
+                                Course = new Course
+                                {
+                                    CourseId = (Guid)reader["CourseId"],
+                                    Name = reader["CourseName"].ToString(),
+                                    Url = reader["Url"].ToString(),
+                                    Par = (int)reader["Par"],
+                                    Rating = reader["Rating"] == DBNull.Value ? (double?)null : Convert.ToDouble(reader["Rating"]),
+                                    Slope = reader["Slope"] == DBNull.Value ? (int?)null : Convert.ToInt32(reader["Slope"])
+                                },
+                                Golfer = new Golfer
+                                {
+                                    GolferId = reader["GolferId"] == DBNull.Value ? Guid.Empty : (Guid)reader["GolferId"],
+                                    Alias = reader["Alias"].ToString(),
+                                    Name = reader["GolferName"].ToString()
+                                },
+                                Points = reader["Points"] == DBNull.Value ? (double?)null : Convert.ToDouble(reader["Points"]),
+                                Score = reader["Score"] == DBNull.Value ? (int?)null : Convert.ToInt32(reader["Score"]),
+                                Par3sWon = reader["Par3sWon"] == DBNull.Value ? (int?)null : (int)reader["Par3sWon"],
+                                WonGame = reader["WonGame"] == DBNull.Value ? false : Convert.ToBoolean(reader["WonGame"]),
+                                Comments = reader["Comments"].ToString()
+                            }); ;
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return seasonRounds;
+        }
+
+        public List<GolferSeasonTotal> GetGolferTotals(string alias)
+        {
+            List<GolferSeasonTotal> seasonTotals = new List<GolferSeasonTotal>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetGolferTotals", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Golfer", alias);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            seasonTotals.Add(new GolferSeasonTotal
+                            {
+                                Golfer = new Golfer
+                                {
+                                    GolferId = reader["GolferId"] == DBNull.Value ? Guid.Empty : (Guid)reader["GolferId"],
+                                    Name = reader["GolferName"].ToString(),
+                                    Alias = reader["Alias"].ToString()
+                                },
+                                Par3Wins = reader["Par3Wins"] == DBNull.Value ? 0 : (int)reader["Par3Wins"],
+                                GameWins = reader["GameWins"] == DBNull.Value ? 0 : (int)reader["GameWins"],
+                                TotalPoints = reader["TotalPoints"] == DBNull.Value ? 0 : Convert.ToDouble(reader["TotalPoints"]),
+                                AverageToPar = reader["AverageToPar"] == DBNull.Value ? (int?)null : (int)reader["AverageToPar"],
+                                Season = (int)reader["Season"]
+                            });
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return seasonTotals;
+        }
+
+        public List<Golfer> GetGolfers()
+        {
+            List<Golfer> golfers = new List<Golfer>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetGolfers", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            golfers.Add(BuildGolfer(reader));
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return golfers;
+        }
+
+        public List<GolferSummary> GetGolferSummaries(int season)
+        {
+            List<GolferSummary> summaries = new List<GolferSummary>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("GetGolfersSummary", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Season", season);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            summaries.Add(new GolferSummary
+                            {
+                                Golfer = BuildGolfer(reader),
+                                CurrentTotals = new GolferTotals
+                                {
+                                    AverageToPar = reader["CurrentAverage"] == DBNull.Value ? (int?)null : (int)reader["CurrentAverage"],
+                                    Par3Wins = reader["CurrentPar3Wins"] == DBNull.Value ? 0 : (int)reader["CurrentPar3Wins"],
+                                    GameWins = reader["CurrentGameWins"] == DBNull.Value ? 0 : (int)reader["CurrentGameWins"]
+                                },
+                                CareerTotals = new GolferTotals
+                                {
+                                    AverageToPar = reader["CareerAverage"] == DBNull.Value ? (int?)null : (int)reader["CareerAverage"],
+                                    Par3Wins = reader["CareerPar3Wins"] == DBNull.Value ? 0 : (int)reader["CareerPar3Wins"],
+                                    GameWins = reader["CareerGameWins"] == DBNull.Value ? 0 : (int)reader["CareerGameWins"]
+                                }
+                            });
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+
+            return summaries;
+        }
+
+        private Golfer BuildGolfer(SqlDataReader reader)
+        {
+            return new Golfer
+            {
+                GolferId = reader["GolferId"] == DBNull.Value ? Guid.Empty : (Guid)reader["GolferId"],
+                Name = reader["Name"].ToString(),
+                Alias = reader["Alias"].ToString(),
+                Avatar = reader["Avatar"] == DBNull.Value ? null : (byte[])reader["Avatar"],
+                Enabled = Convert.ToBoolean(reader["Enabled"]),
+                BringsBeer = Convert.ToBoolean(reader["BringsBeer"])
+            };
+        }
+    }
+}
