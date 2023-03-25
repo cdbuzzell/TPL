bd5369d032150118a7e3e3533b8bc37ca88c305f
Corey Buzzell <cdbuzzell@gmail.com>
2023-03-24 21:55:02 -0500

    will we do unit tests and a mobile app?!

---
+++ TPL.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TPL.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TPL.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TPL.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
